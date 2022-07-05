cc76a7153a174c2c7fc636045859e5be88b8bc1d
vasylkrupa <vkrupa@intelliarts.com>
2022-07-05 17:13:00 +0300

    subXRWH-253 Fix pagination2

--- plugins/rocks.xml.webhelp/resources/js/xml.rocks.search-page.js
+++ plugins/rocks.xml.webhelp/resources/js/xml.rocks.search-page.js
@@ -2,6 +2,26 @@ var url_string = window.location.href,
     url = new URL(url_string),
     key = url.searchParams.get("key");
 
+
+var idx = lunr(function () {
+    this.ref('name')
+    this.field('text')
+
+    documents.forEach(function (doc) {
+        this.add(doc);
+    }, this)
+})
+
+var searchInputValue = $('#body-search-input').val();
+var searchRes = idx.search(searchInputValue);
+
+// Add search document number
+var searchDocNumber = document.getElementById("search-documents-number");
+var searchDocNumberValue = document.createTextNode(searchRes.length);
+searchDocNumber.appendChild(searchDocNumberValue);
+
+var i = 0;
+
 $(document).ready(function() {
     // Press body search button on enter
     // Get the input field
@@ -15,6 +35,42 @@ $(document).ready(function() {
             document.getElementById("body-search-button").click();
         }
     });
+
+    (function(name) {
+        var container = $('#pagination-' + name);
+        container.pagination({
+            dataSource: searchRes,
+            locator: searchRes,
+            totalNumber: 50,
+            pageSize: 10,
+            showPageNumbers: true,
+            showPrevious: true,
+            showNext: true,
+            showNavigator: true,
+            showFirstOnEllipsisShow: true,
+            showLastOnEllipsisShow: true,
+            ajax: {
+                beforeSend: function() {
+                    container.prev().html('Loading data from flickr.com ...');
+                }
+            },
+            callback: function(response, pagination) {
+                var dataHtml = '<div class="search-result-block">';
+
+                $.each(response, function (index, item) {
+                    var itemRef = item.ref;
+                    var searchResultHref = documents.filter(iT => iT.name === itemRef).map(iT => iT.href);
+                    var searchResultBodyText = documents.filter(iT => iT.name === itemRef).map(iT => iT.text);
+
+                    dataHtml += '<p class="search-result-title"><a href="'+ searchResultHref +'">' + item.ref + '</a></p>';
+                    dataHtml += '<p class="search-result-body">' + searchResultBodyText + '</p>';
+                });
+
+                dataHtml += '</div>';
+                container.prev().html(dataHtml);
+            }
+        })
+    })('demo2');
 });
 
 // If keyword exists show 'document(s) found for [keyword]' text
@@ -33,74 +89,3 @@ if(key !== '') {
     // Show string 'Search keyword cannot be empty'
     document.getElementsById('empty-keyword').classList.add('show');
 }
-
-var idx = lunr(function () {
-    this.use(lunr.ar);
-    this.use(lunr.da);
-    this.use(lunr.de);
-    this.use(lunr.du);
-    this.use(lunr.es);
-    this.use(lunr.fi);
-    this.use(lunr.fr);
-    this.use(lunr.hi);
-    this.use(lunr.it);
-    this.use(lunr.ja);
-    this.use(lunr.jp);
-    this.use(lunr.nl);
-    this.use(lunr.no);
-    this.use(lunr.pt);
-    this.use(lunr.ro);
-    this.use(lunr.ru);
-    this.use(lunr.sv);
-    this.use(lunr.ta);
-    this.use(lunr.th);
-    this.use(lunr.tr);
-    this.use(lunr.vi);
-    this.use(lunr.zh);
-
-    this.ref('name')
-    this.field('text')
-
-    documents.forEach(function (doc) {
-        this.add(doc);
-    }, this)
-})
-
-var searchInputValue = $('#body-search-input').val();
-var searchRes = idx.search(searchInputValue);
-/*console.log(searchRes);
-console.log(searchRes.length);*/
-
-// Add search document number
-var searchDocNumber = document.getElementById("search-documents-number");
-var searchDocNumberValue = document.createTextNode(searchRes.length);
-searchDocNumber.appendChild(searchDocNumberValue);
-
-var i = 0;
-
-searchRes.forEach(function () {
-    /*console.log(searchRes[i].ref);*/
-    /*console.log(documents[i]["text"]);*/
-    /*console.log(documents.filter(item => item.name === searchRes[i].ref).map(item => item.text));*/
-
-    var searchResultBlock = document.createElement("div");
-    var searchResultTitle = document.createElement("p");
-    var anchor = document.createElement("a");
-    var searchResultTitleText = document.createTextNode(searchRes[i].ref);
-
-    searchResultBlock.classList.add('search-result-block');
-    searchResultBlock.appendChild(searchResultTitle).classList.add('search-result-title');
-    searchResultTitle.appendChild(anchor);
-    anchor.appendChild(searchResultTitleText);
-    anchor.href = documents.filter(item => item.name === searchRes[i].ref).map(item => item.href);
-
-    var searchResultBody = document.createElement("p");
-    var searchResultBodyText = document.createTextNode(documents.filter(item => item.name === searchRes[i].ref).map(item => item.text));
-    searchResultBlock.appendChild(searchResultBody).classList.add('search-result-body');
-    searchResultBody.appendChild(searchResultBodyText);
-
-    var searchResults = document.getElementById("search-results");
-    searchResults.appendChild(searchResultBlock);
-
-    i++;
-}, this)
